1d65624c30a4e45740046d2ea3725a95b4739ec1
jmcniel2000 <jmcniel@desmet.org>
2017-01-23 10:17:13 -0600

    Update login.php

--- UPDATED HTML/login.php
+++ UPDATED HTML/login.php
@@ -12,18 +12,6 @@
 </head>
 
 <body>
-<?php
-$servername = "127.0.0.1";
-$username = "root";
-$password = "";
-$dbname = "sparta";
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
-// Check connection
-if (!$conn) {
-     die("Connection failed: " . mysqli_connect_error());
-}
-?>
 <div class="container-fluid">
 
 
