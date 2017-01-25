b6aa64b0a6cc9a998dbfcc25860a7c12f881c4e9
mmeyer117 <mmeyer@desmet.org>
2017-01-25 11:09:20 -0600

    Add files via upload

---
+++ UPDATED HTML/congrats.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <title>Spartan Service</title>
+  <link rel="icon" type="image/x-icon" href="Media/favicon.ico" alt="A picture of the De Smet Crest" />
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  
+  <link rel="stylesheet" href="css/BS.css">
+  <link rel="stylesheet" href="css/SpartanCSS.css">
+</head>
+
+<body>
+<?php
+if(!isset($_COOKIE["DS_UserID"]))
+{
+	header( 'Location: login.php' ) ;
+}
+?>
+<div class="container-fluid">
+
+
+	<div class="well well-sm">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-lg-2">
+					<img src="Media/DSJH80.png" alt="De Smet Logo" align=left class="img-responsive" height=80px>
+				</div>
+				<div class="col-lg-8">
+					<img src="Media/SpartanService.png" alt="Spartan Service Logo"  align=center class="img-responsive" height=80px> 
+				</div>
+				<div class="col-lg-2">	
+					<img src="Media/AMDG2s.png" alt="De Smet Logo" align=right class="img-responsive" height=80px> 
+				</div>
+			</div>			
+		</div>
+	</div>
+
+
+	<div class="row">
+		<div class="col-sm-2 col-md-2 col-lg-2"> 
+			<ul class="nav nav-pills nav-stacked">
+				<li><a href="index.php">Home</a></li>
+				<li><a href="Service.php">Service Projects</a></li>
+				<li><a href="MyProfile.php">My Profile</a></li>
+				<?php
+					if ($_COOKIE["DS_UserTypeID"] == 2 or $_COOKIE["DS_UserTypeID"] == 0)
+					print "<li><a href='addevent.php'>Add Event</a></li>";
+				?>
+			</ul>
+<br>
+			<ul class="nav nav-pills nav-stacked">
+				<?php
+					if(isset($_COOKIE["DS_UserID"]))
+					{
+						print "<li><h4>Welcome ".$_COOKIE["DesmetFirstName"]."</h4></li>";
+						print "<form action=\"logoutScript.php\" class=\"form-horizontal\"> <div class=\"col-sm-12 text-center\">
+					<button type=\"submit\" class=\"btn btn-default\"><strong>Logout</strong></button>
+					<br>
+				  </div><br>
+				</form><br> ";
+					}
+					else
+					{
+						print "<li><a href='login.php'>Login</a></li>";
+					}
+					
+				?>
+			</ul> 			
+			<br>
+			<br>
+		</div>
+
+		<div class="col-sm-10 col-md-10 col-lg-10"> 
+			<div class="container-fluid">
+					<h1> Congrats! </h1>
+					 
+					<h2> Thank you for signing up for this event 
+					<?php echo  $_COOKIE["DesmetFirstName"]; echo $_COOKIE["DS_UserTypeID"]; ?>! We greatly appreciate your support. 
+ </h2>
+				
+  </div>
+</div>
+				
+			</div>
+		</div>
+	</div> 
+	
+<br>
+<footer class="container-fluid text-center">
+<div class="row">
+	<div class="col-sm-2">
+		<img src="Media/CRESTs.png" alt="De Smet Crest" class="img-responsive" height=80px align=center>
+	</div>
+	<div class="col-sm-8">
+		<b> <span style="font-size: 20px">De Smet Jesuit High School</span> </b> <hr width=90%>
+233 North New Ballas Rd. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; St. Louis, MO 63141 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Contact Us: <a href="mailto:SpartanService@desmet.org"> SpartanService@desmet.org 
+	</div>
+	<div class="col-sm-2">
+		<img src="Media/SHs.png" alt="De Smet Logo" class="img-responsive" height=80px align=center>
+	</div>	
+</div>
+</footer>
+
+</div>
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>	
+  <script src="js/bootstrap.min.js"></script>
+  
+</body>
+</html>
+
+
+
